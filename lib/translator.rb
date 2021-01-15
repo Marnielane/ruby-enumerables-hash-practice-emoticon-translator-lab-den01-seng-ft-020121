5e9f96bbcd023aa0f10f6af782e5a75bca6dc958
Marnie Robinson <marnielane@gmail.com>
2021-01-15 18:59:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ end
 def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
       if value[:english] == emoticon
-        return key
+        return value
       end
   
   end
